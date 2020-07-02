9586ae138f09b21ae317608261b72c60e5d22a99
vishal <vishal3bhati@gmail.com>
2020-07-02 16:29:20 +0530

    Bug: Add video resolution only if its supported

--- android/src/main/java/com/google/android/cameraview/Camera1.java
+++ android/src/main/java/com/google/android/cameraview/Camera1.java
@@ -1117,7 +1117,7 @@ class Camera1 extends CameraViewImpl implements MediaRecorder.OnInfoListener,
         if(videoResolution!=null && videoResolution.hasKey("videoFrameWidth") && videoResolution.hasKey("videoFrameHeight")){
             int videoFrameWidth = videoResolution.getInt("videoFrameWidth");
             int videoFrameHeight = videoResolution.getInt("videoFrameHeight");
-            if(videoFrameWidth > 0 && videoFrameHeight > 0) {
+            if(videoFrameWidth > 0 && videoFrameHeight > 0 && mPictureSizes.sizes(mAspectRatio).contains(new Size(videoFrameWidth, videoFrameHeight))) {
                 mMediaRecorder.setVideoSize(videoFrameWidth, videoFrameHeight);
             } else {
                 mMediaRecorder.setVideoSize(profile.videoFrameWidth, profile.videoFrameHeight);
